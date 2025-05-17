17996761749729729c754f2915fba42da95ba060
Bharat Bhangale <cs.bharat.dev@gmail.com>
2025-05-17 11:35:20 +0530

    Add Redux setup with chat slice and integrate store in main application

---
+++ client/src/redux/features/chatSlice.jsx
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+
+const chatSlice = createSlice({
+    name: "chat",
+    initialState: {}, 
+    reducers:{}
+})
+
+
+export default chatSlice.reducer;
